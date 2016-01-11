cc2bbd0965dc67a3adc70d50a1145f745dbccbc8
Joseph Pachod <joseph.pachod@berger-levrault.fr>
2016-01-11 23:07:02 +0100

    working again - cqrs4js to wire

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, DefaultRoute } from 'react-router';
 
 import {EventBus, CommandBus, Provider, ViewRegister} from './cqrs4js/cqrs4js';
 
@@ -15,28 +14,11 @@ const viewRegister = new ViewRegister();
 
 viewRegister.register('conversationsView', createConversationsView(eventBus));
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div >
-        <Router>
-          <Route name="home" path="/" handler={App}>
-            <DefaultRoute handler={ConversationsUi}/>
-          </Route>
-        </Router>
-      </div>
-    );
-  }
-}
-
-console.log(ReactDOM);
-
 ReactDOM.render(
   <Provider eventBus={eventBus}
             commandBus={commandBus}
             viewRegister={viewRegister}>
-    <App />
+    <ConversationsUi/>
   </Provider>,
   document.getElementById("app")
 );
